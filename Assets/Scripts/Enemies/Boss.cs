32f335e8a0b5cb03f1acd66e4dce4bf532421338
unknown <stamandtyler@gmail.com>
2022-04-23 01:45:55 -0400

    Deleted unused script

---
+++ Assets/Scripts/Enemies/Boss.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Has different phases
+    //Phase 1. Charges player and hits wall, wall turns to electicity
+    //Phase 2. Starts shooting player after charging
+    //Phase 3. Charging is faster
+    
+
+    //Charging:
+        //Checks wall gravity is on
+    //Shoots player from chest
+
+}
